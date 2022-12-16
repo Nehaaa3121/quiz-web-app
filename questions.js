8db9b467b481dbe30511b093abf7dc1de14e9a08
Nehaa3121 <nehachoudhary3198@gmail.com>
2022-12-16 15:12:39 +0530

    quiz-web-app

---
+++ questions.js
@@ -0,0 +1,333 @@
+let questions = [
+    {
+     // question 1
+      numb: 1,
+      question: "What Does Css Stand for?",
+      answer: "Cascading style sheet",
+      options:[
+          "Crystal style sheet",
+          "Common style sheet",
+          "cascaded style sheet",
+          "Cascading style sheet",
+      ]
+  },
+  {
+    //question2
+      numb: 2,
+      question: "Whic of the following is a block-line element ?",
+      answer: "Div",
+      options:[
+          "Div",
+          "Span",
+          "Br",
+          "iframe",
+      ]
+  },
+  { 
+    //question3
+      numb: 3,
+      question: "For what purpose Blockquote is used in html?",
+      answer: "To separate sections of text",
+      options:[
+          "To separate sections of text",
+          "To block the paragraph",
+          "To delete the paragraph",
+          "To write the quote",
+      ]
+  },
+  {
+    //question4
+      numb: 4,
+      question: "Who is making the Web standards? ",
+      answer: "The World Wide Web Consortium",
+      options:[
+          "Google",
+          "The World Wide Web Consortium",,
+          "Mozila",
+          "Microsoft",
+      ]
+  },
+
+    {
+
+      //question5
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language",
+    ]
+
+
+},
+{
+
+  //question6
+ 
+  numb: 6,
+   question: "How do you round the number 7.25 to thr nearest integer?",
+
+  answer: "Math.round(7.25)",
+    options:[
+      "Math.rnd(7.25)",
+
+      "Math.round(7.25)",
+      " rnd(7.25)",
+      " round (7.25)",
+
+  ]
+},
+{
+
+  //question7
+  numb: 7,
+  question: "DOM stands for?",
+
+  answer: "Document Object Model",
+
+  options:[
+    "Desktop Oriented Modle",
+    "Drive Object Model",
+    "Dontcare Object Model",
+     "Document Object Model",
+
+  ]
+},
+{
+
+  //question8
+  numb: 8,
+  question: "Graphics defined by svg in which format?",
+  answer: "XML",
+  options:[
+      "XML",
+      "CSS",
+      "HTML",
+      "JavaScript",
+  ]
+},
+{
+  //question9
+  numb: 9,
+  question: "The HTML canvas elemnt is used to for?",
+  answer: "draw graphics",
+  options:[
+      "draw graphics",
+      "display data",
+      "amnipulate",
+      "draggble elements",
+  ]
+},
+{
+
+  //question10
+  numb: 10,
+  question: "Which input type defines a slider control",
+  answer: "Range",
+  options:[
+      "Range",
+      "search",
+      "controls",
+      "slider",
+  ]
+},
+{
+  //question11
+  numb: 11,
+  question: "What Does HTML stands for?",
+  answer: "Hyper text markup language",
+  options:[
+      "Hyper text markup language",
+      "Hyper text mark link",
+      "Home tool mark language",
+      "Helium tool mark larsentube",
+  ]
+},
+{
+
+  //question12
+  numb: 12,
+  question: "Which HTML attribute is used to define inline styles?",
+
+
+  answer: "Style",
+  options:[
+      "Style",
+      "class",
+      "font",
+      "id",
+  ]
+},
+{
+  //question13
+  numb: 13,
+  question: "how do you insert acomment in a Css file?",
+  answer: "/*hello*/",
+  options:[
+      "/*hello*/",
+      " ' hello ",
+      "// hello",
+      "//hello .//",
+  ]
+},
+{
+  //question14
+
+  numb: 14,
+  question: "Is JavaScript is case sensitive?",
+  answer: "Yes",
+
+  options:[
+    "Yes",
+    "No",
+    "Neither yes nor no",
+    "May be",
+
+  ]
+},
+{
+  //question15
+  numb: 15,
+  question: "What is the default value of the position property?",
+  answer: "static",
+  options:[
+      "static",
+      "absolute",
+      "relative",
+      "fixed",
+  ]
+},
+{
+  //question16
+  numb: 16,
+  question: "how do you make the text bold",
+  answer: "Font-weight:bold;",
+  options:[
+      "Font-weight:bold;",
+      "bold",
+      "style:bold",
+      "font:bold",
+  ]
+},
+{
+  //question17
+  numb: 17,
+  question: "How do you select elements with class anme 'test?",
+  answer: ".test",
+  options:[
+      ".test",
+      "#test",
+      "//test",
+      "*test",
+  ]
+},
+{
+
+  //question18
+  numb: 18,
+  question: "how to decalare a varaiable in javascript",
+        answer: "var x;",
+        options:[
+            "var x;",
+            "varaible x",
+            "v x;",
+            "var = x;",
+
+        ]
+},
+{
+
+  //question19
+  numb: 19,
+  question: "How do you create a funtion in javascript",
+  answer: "Function myFunction()",
+  options:[
+      "Function myFunction()",
+      "function()",
+      "function:",
+      "func()",
+  ]
+},
+{
+
+  //question20
+  numb: 20,
+  question: "how to write an if statement in javascript",
+  answer: "if (i==5)",
+  options:[
+      "if (i==5)",
+      "if i=5;",
+      "if i=5 then",
+      "if i==5 then",
+  ]
+},
+{
+  //question21
+  numb: 21,
+  question: "what is the correct way to write a array?",
+  answer: "var x = [10,20,30]",
+  options:[
+      "var x = [10,20,30]",
+      "var x 1= 10 , 2=20, 3=30",
+      "var x = 10 ,20,30",
+      "var x = (10,20,30)",
+  ]
+
+},
+{
+  //question22
+  numb: 22,
+  question: "Which operator is used to assign a value to varaiable?",
+  answer: " equal to operator",
+  options:[
+      "divison operator",
+      "equal to operator",
+      "multiply opeartor",
+      "not equal to",
+  ]
+},
+{
+  //question23
+  numb: 23,
+  question: "Which event occurs when the user clicks on a HTml element?",
+  answer: "On click",
+  options:[
+      "On click",
+      "On change",
+      "On mouseclick",
+      "on mousehover",
+  ]
+},
+{
+  //question24
+  numb: 24,
+  question: "How do you select an elment with id 'demo?",
+  answer: "#demo",
+  options:[
+      "#demo",
+      ".demo",
+      "*demo",
+      "demo",
+  ]
+},
+
+{
+  //question 25;
+  numb: 25,
+  question: "Which Css property controls the text-size?",
+  answer: "font-size",
+  options:[
+      "font-size",
+      "text-size",
+      "text-style",
+      "font-style",
+  ]
+},
+
+
+
+
+    ]
